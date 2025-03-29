1f3fef50dc81b1d7dcacff727a0fed2cce0077db
ikrameEL HAITI <ikrameelhaiti@gmail.com>
2025-03-29 18:43:27 +0000

    Créer une implémentation de cette interface

--- Act-N1-Injection-des-dependances/src/main/java/net/ikrame/pres/pres2.java
+++ Act-N1-Injection-des-dependances/src/main/java/net/ikrame/pres/pres2.java
@@ -5,6 +5,7 @@ import net.ikrame.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class pres2 {
@@ -18,7 +19,10 @@ public class pres2 {
 
         String metierClassName = scanner.nextLine();
         Class cMetier = Class.forName(metierClassName);
-        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(d);
+//        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(d);
+//        IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+//        Method setDao = cMetier.getDeclaredMethod("setDao",IDao.class);
+//        setDao.invoke(metier,d);
 
         System.out.println("Res ="+metier.calcul());
     }
